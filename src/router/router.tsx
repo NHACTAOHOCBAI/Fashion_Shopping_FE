8d93cc16d99671e4f0f41dd051f9a28744497409
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-07-09 21:48:14 +0700

    done side bar

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Category from "../pages/admin/category/Category";
 import AdminLayout from "../layouts/AdminLayout";
+import Dashboard from "../pages/admin/dashboard/Dashboard";
 const router = createBrowserRouter([
     {
         path: "/admin",
         element: <AdminLayout />,
         children: [
+            {
+                path: "dashboard",
+                element: <Dashboard /> // Placeholder for Category component
+            },
             {
                 path: "categories",
                 element: <Category /> // Placeholder for Category component
