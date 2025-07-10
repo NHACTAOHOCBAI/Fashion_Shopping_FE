dbaccb804afa4156a770ca772fbcd22763502ce9
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-07-10 08:37:05 +0700

    revise sidebar

--- src/layouts/admin_layouts/Sidebar.tsx
+++ src/layouts/admin_layouts/Sidebar.tsx
@@ -15,20 +15,20 @@ interface SidebarProps {
 }
 const Sidebar = ({ isCollapsed, activeItem, items }: SidebarProps) => {
     return (
-        <div className={`bg-white flex flex-col gap-[10px] shadow-md border-r-2  transition-all duration-[400ms] ${isCollapsed ? 'w-16' : 'w-[200px]'}`}>
+        <div className={`bg-white flex flex-col gap-[6px] shadow-md border-r-2  transition-all duration-[400ms] ${isCollapsed ? 'w-16' : 'w-[190px]'}`}>
             {
                 items.map((group) => (
                     <div key={group.groupName}>
-                        <h3 className={`text-text-heading font-heading uppercase px-[10px] py-[4px] transition-opacity duration-[400ms] ease-in-out ${isCollapsed ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
+                        <h3 className={`text-text-heading font-heading uppercase px-[10px] py-[2px] transition-opacity duration-[400ms] ease-in-out ${isCollapsed ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
                             {group.groupName}
                         </h3>
-                        <ul className='flex flex-col gap-[4px]'>
+                        <ul className='flex flex-col gap-[2px]'>
                             {group.items.map((item) => (
-                                <li key={item.key} className='flex'>
-                                    <div className={`h-[48px] transition-all duration-[400ms]  rounded-[4px] bg-accent-pinkRed ${activeItem === item.key ? 'opacity-100 w-[6px]' : 'opacity-0 w-0 '}`}></div>
+                                <li key={item.key} className='flex h-fit'>
+                                    <div className={` transition-all duration-[400ms]  rounded-[4px] bg-accent-pinkRed ${activeItem === item.key ? 'opacity-100 w-[6px]' : 'opacity-0 w-0 '}`}></div>
                                     <Link
                                         to={item.key}
-                                        className={`flex mx-[10px] items-center w-[160px] p-[12px] rounded-[8px] transition duration-[400ms] ${activeItem === item.key ? 'bg-accent-pinkRed text-white' : 'text-text-body hover:text-accent-pinkRed '}`}
+                                        className={`flex mx-[10px] items-center w-[160px] p-[10px] rounded-[8px] transition duration-[400ms] ${activeItem === item.key ? 'bg-accent-pinkRed text-white' : 'text-text-body hover:text-accent-pinkRed '}`}
                                     >
                                         <span >{item.icon}</span>
                                         <span
